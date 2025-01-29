b948a82948d4822bc7cc6e2921be0ac36a5a0c8a
Guillem Forns <guillem.fo@gmail.com>
2025-01-29 19:31:49 +0100

    ok

--- srcs/requirements/wordpress/tools/wp_script.sh
+++ srcs/requirements/wordpress/tools/wp_script.sh
@@ -6,7 +6,7 @@ then
 	wp core download --allow-root
 	wp config create --dbname=$MYSQL_DBS --dbuser=$MYSQL_USER --dbpass=$MYSQL_USER_PASS --dbhost=$MYSQL_HOSTNAME --allow-root
 	wp core install --url=$DOMAIN_NAME --title=$WORDPRESS_TITLE --admin_user=$WORDPRESS_ADMIN --admin_password=$WORDPRESS_ADMIN_PASS --admin_email=$WORDPRESS_EMAIL --skip-email --allow-root
-	wp user create $WORDPRESS_USER $WORDPRESS_EMAIL --role=author --user_pass=$WORDPRESS_USER_PASS --allow-root
+	wp user create $WORDPRESS_USER $WORDPRESS_USER_EMAIL --role=author --user_pass=$WORDPRESS_USER_PASS --allow-root
 	wp theme install twentytwentytwo --activate --allow-root
 fi
 
